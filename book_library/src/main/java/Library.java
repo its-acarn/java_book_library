baa2bb1a31c242c3b5d4dd22868a202bd9fa6c2a
svvishjs <andreww92@live.com>
2020-11-17 18:04:10 +0000

    finished MVP

--- book_library/src/main/java/Library.java
+++ book_library/src/main/java/Library.java
@@ -2,9 +2,11 @@ import java.util.ArrayList;
 
 public class Library {
 
+    private int capacity;
     private ArrayList<Book> library;
 
-    public Library() {
+    public Library(int capacity) {
+        this.capacity = capacity;
         this.library = new ArrayList<>();
     }
 
@@ -12,4 +14,8 @@ public class Library {
         return this.library.size();
     }
 
+    public void addBook(Book book) {
+        if(this.capacity > countBooks())
+            this.library.add(book);
+    }
 }
--- book_library/src/test/java/LibraryTest.java
+++ book_library/src/test/java/LibraryTest.java
@@ -6,14 +6,22 @@ import static org.junit.Assert.assertEquals;
 public class LibraryTest {
 
     Library library;
+    Book bible;
 
     @Before
     public void before() {
-        library = new Library();
+        library = new Library(2);
+        bible = new Book("Bible", "Various", "Religion");
     }
 
     @Test
     public void checkBookCountIsZero() {
         assertEquals(0, library.countBooks());
+    }
+
+    @Test
+    public void canAddBook() {
+        library.addBook(bible);
+        assertEquals(1, library.countBooks());
     }
 }
